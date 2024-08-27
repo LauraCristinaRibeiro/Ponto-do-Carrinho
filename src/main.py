297716691e56a87efbc667d860f3d1891b3d3293
Lucas <lucas.r.s.s.2031@gmail.com>
2024-08-27 15:09:27 -0300

    configura integração entre as funcionalidades

--- src/main.py
+++ src/main.py
@@ -1,1 +1,23 @@
+import tkinter as tk
+
+from login import tela_login
+
+from cadastro_users import tela_cadastro
+def tela_pincipal():
+    # Create the main window
+    tela_inicial = tk.Tk()
+    tela_inicial.title("Tela Inicial")
+
+    # Create a button that will call the tela_login function when clicked
+    btn_inicializar_login = tk.Button(tela_inicial, text="Iniciar Login", command=tela_login)
+    btn_inicializar_login.pack(pady=10)
+
+    btn_inicializar_login = tk.Button(tela_inicial, text="cadastro", command=tela_cadastro)
+    btn_inicializar_login.pack(pady=10)
+
+    # Start the Tkinter event loop
+    tela_inicial.mainloop()
+
+if __name__ == "__main__":
+    tela_pincipal()
 
