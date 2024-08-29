e6430ccab8c228daef87eea173f50dd052d5af22
Lucas <lucas.r.s.s.2031@gmail.com>
2024-08-29 16:02:28 -0300

    criação do botão perfil

--- src/perfil.py
+++ src/perfil.py
@@ -1,36 +1,52 @@
 import tkinter as tk
 from tkinter import ttk
+from firebase_config import recuperar_user
 
 
-root = tk.Tk()
+def mostrar_perfil(user_id):
+    user_data = recuperar_user(user_id)
+    if (user_data):
+        root = tk.Tk()
+        root.title('Perfil')
 
 
-root.title('Perfil')
+        label_nome = tk.Label(root,text=f"Nome: {user_data['nome']}")
+        label_nome.pack(pady=10)
 
 
-window_width = 1000
-window_height = 1000
+        label_email = tk.Label(root,text=f"Email: {user_data['email']}")
+        label_email.pack(pady=10)
 
 
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
+        label_cargo = tk.Label(root,text=f"Cargo: {user_data['cargo']}")
+        label_cargo.pack(pady=10)
+
+        window_width = 1000
+        window_height = 1000
 
 
-center_x = int(screen_width//2 - window_width//2)
-center_y = int(screen_height//2 - window_height//2)
+        screen_width = root.winfo_screenwidth()
+        screen_height = root.winfo_screenheight()
 
 
-root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+        center_x = int(screen_width//2 - window_width//2)
+        center_y = int(screen_height//2 - window_height//2)
 
 
-root.resizable(False, False)
+        root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 
 
-root.iconbitmap('.//assets/ic_ponto_do_carrinho.ico')
+        root.resizable(False, False)
 
 
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text='').grid(column=0, row=0)
+        root.iconbitmap('.//assets/ic_ponto_do_carrinho.ico')
 
-root.mainloop()
+
+        frm = ttk.Frame(root, padding=10)
+        frm.grid()
+        ttk.Label(frm, text='').grid(column=0, row=0)
+
+        root.mainloop()
+
+    else:
+        print("Erro ao recuperar dados do usuário")
