3ae178327855ab1752d8844589f8b0ebae8e50ae
Lucas <lucas.r.s.s.2031@gmail.com>
2024-08-29 17:07:54 -0300

    corrige abertura da tela cadastro

--- src/cadastro_produtos.py
+++ src/cadastro_produtos.py
@@ -1,48 +1,45 @@
 import tkinter as tk
 from tkinter import messagebox
-telaCadProdutos = tk.Tk()
-
-entrada1 = tk.StringVar()
-entrada2 = tk.StringVar()
-entrada3 = tk.StringVar()
-resultado = tk.StringVar()
-
-telaCadProdutos.title('PRODUTOS PONTO DO CARRINHO')
-
-tk.Label(telaCadProdutos, text="Cadastrar Produto: ").grid(row=0, column=0)
-tk.Entry(telaCadProdutos, textvariable=entrada1).grid(row=0, column=1)
-
-tk.Label(telaCadProdutos, text="Cadastrar Marca: ").grid(row=1, column=0)
-tk.Entry(telaCadProdutos, textvariable=entrada2).grid(row=1, column=1)
-
-tk.Label(telaCadProdutos, text="Cadastrar Preço: ").grid(row=2, column=0)
-tk.Entry(telaCadProdutos, textvariable=entrada3).grid(row=2, column=1)
-
-tk.Button(telaCadProdutos, text="Confirmar")
 
 def realizar_confirmar():
-
     try:
        # user = auth.sign_in_with_email_and_password(email, senha)
         messagebox.showinfo("SUCESSO", "PRODUTO CADASTRADO COM SUCESSO!")
     except:
         messagebox.showerror("ERRO", "FALHA AO CADASTRAR PRODUTO")
 
+def realizar_cadastro():
+    entrada1 = tk.StringVar()
+    entrada2 = tk.StringVar()
+    entrada3 = tk.StringVar()
+    resultado = tk.StringVar()
 
-btn_confirmar = tk.Button(telaCadProdutos, text="CONFIRMAR", command=realizar_confirmar)
-btn_confirmar.grid(row=3, column=1, pady=10)
+    realizar_cadastro = tk.Toplevel()
 
-window_width = 300
-window_height = 200
+    tk.Label(realizar_cadastro, text="Cadastrar Produto: ").grid(row=0, column=0)
+    tk.Entry(realizar_cadastro, textvariable=entrada1).grid(row=0, column=1)
 
-screen_width = telaCadProdutos.winfo_screenwidth()
-screen_height = telaCadProdutos.winfo_screenheight()
+    tk.Label(realizar_cadastro, text="Cadastrar Marca: ").grid(row=1, column=0)
+    tk.Entry(realizar_cadastro, textvariable=entrada2).grid(row=1, column=1)
 
-center_x = int(screen_width//2 - window_width//2)
-center_y = int(screen_height//2 - window_height//2)
+    tk.Label(realizar_cadastro, text="Cadastrar Preço: ").grid(row=2, column=0)
+    tk.Entry(realizar_cadastro, textvariable=entrada3).grid(row=2, column=1)
 
-telaCadProdutos.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+    tk.Button(realizar_cadastro, text="Confirmar")
+    btn_confirmar = tk.Button(realizar_cadastro, text="CONFIRMAR", command=realizar_confirmar)
+    btn_confirmar.grid(row=3, column=1, pady=10)
 
-telaCadProdutos.resizable(False, False)
+    window_width = 300
+    window_height = 200
 
-telaCadProdutos.mainloop()
+    screen_width = realizar_cadastro.winfo_screenwidth()
+    screen_height = realizar_cadastro.winfo_screenheight()
+
+    center_x = int(screen_width//2 - window_width//2)
+    center_y = int(screen_height//2 - window_height//2)
+
+    realizar_cadastro.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+
+    realizar_cadastro.resizable(False, False)
+
+    realizar_cadastro.mainloop()
