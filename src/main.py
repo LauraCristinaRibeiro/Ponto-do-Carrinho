76aeb628927a52c6b8c01076ca5c4a3284863c54
Lucas <lucas.r.s.s.2031@gmail.com>
2024-09-02 16:03:38 -0300

    correção de codigo

--- src/main.py
+++ src/main.py
@@ -73,67 +73,63 @@ from consulta_produtos import PesquisaWindow
 from perfil import PerfilWindow
 
 def main():
-root = tk.Tk()
-root.title(&quot;Ponto do Carrinho&quot;)
-root.geometry(&quot;400x400&quot;)
+    root = tk.Tk()
+    root.title("Ponto do Carrinho")
+    root.geometry("400x400")
 
-def on_login_success(user_data):
-for widget in root.winfo_children():
-widget.destroy()
+    def on_login_success(user_data):
+        for widget in root.winfo_children():
+            widget.destroy()
 
-tk.Label(root, text=f&quot;Bem-vindo, {user_data[&#39;name&#39;]}!&quot;).pack()
+        tk.Label(root, text=f"Bem-vindo, {user_data['name']}!").pack()
 
-perfil_button = tk.Button(root, text=&quot;Perfil&quot;, command=lambda:
-open_perfil(user_data))
-perfil_button.pack()
+        perfil_button = tk.Button(root, text="Perfil", command=lambda: open_perfil(user_data))
+        perfil_button.pack()
 
-cadastro_produtos_button = tk.Button(root, text=&quot;Cadastrar Produto&quot;,
-command=open_cadastro_produtos)
-cadastro_produtos_button.pack()
+        cadastro_produtos_button = tk.Button(root, text="Cadastrar Produto", command=open_cadastro_produtos)
+        cadastro_produtos_button.pack()
 
-pesquisa_button = tk.Button(root, text=&quot;Pesquisar Produto&quot;,
-command=open_pesquisa)
-pesquisa_button.pack()
+        pesquisa_button = tk.Button(root, text="Pesquisar Produto", command=open_pesquisa)
+        pesquisa_button.pack()
 
-def open_login():
-for widget in root.winfo_children():
-widget.destroy()
-LoginWindow(root, on_login_success).pack()
+    def open_login():
+        for widget in root.winfo_children():
+            widget.destroy()
+        LoginWindow(root, on_login_success).pack()
 
-def open_cadastro_users():
-for widget in root.winfo_children():
-widget.destroy()
-CadastroUsersWindow(root).pack()
+    def open_cadastro_users():
+        for widget in root.winfo_children():
+            widget.destroy()
+        CadastroUsersWindow(root).pack()
 
-def open_cadastro_produtos():
-for widget in root.winfo_children():
-widget.destroy()
-CadastroProdutosWindow(root).pack()
+    def open_cadastro_produtos():
+        for widget in root.winfo_children():
+            widget.destroy()
+        CadastroProdutosWindow(root).pack()
 
-def open_pesquisa():
-for widget in root.winfo_children():
-widget.destroy()
-PesquisaWindow(root).pack()
+    def open_pesquisa():
+        for widget in root.winfo_children():
+            widget.destroy()
+        PesquisaWindow(root).pack()
 
-def open_perfil(user_data):
-for widget in root.winfo_children():
-widget.destroy()
-PerfilWindow(root, user_data).pack()
+    def open_perfil(user_data):
+        for widget in root.winfo_children():
+            widget.destroy()
+        PerfilWindow(root, user_data).pack()
 
-# Menu Principal
-login_button = tk.Button(root, text=&quot;Login&quot;, command=open_login)
-login_button.pack()
+    # Menu Principal
+    login_button = tk.Button(root, text="Login", command=open_login)
+    login_button.pack()
 
-cadastro_users_button = tk.Button(root, text=&quot;Cadastrar Usuário&quot;,
-command=open_cadastro_users)
-cadastro_users_button.pack()
+    cadastro_users_button = tk.Button(root, text="Cadastrar Usuário", command=open_cadastro_users)
+    cadastro_users_button.pack()
 
-pesquisa_button = tk.Button(root, text=&quot;Pesquisar Produto&quot;,
-command=open_pesquisa)
-pesquisa_button.pack()
+    pesquisa_button = tk.Button(root, text="Pesquisar Produto", command=open_pesquisa)
+    pesquisa_button.pack()
 
-root.mainloop()
+    root.mainloop()
 
-if __name__ == &quot;__main__&quot;:
-main()
+if __name__ == "__main__":
+    main()
+
 
